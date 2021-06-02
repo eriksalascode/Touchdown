6d8e0acfa58dae6301bba57dfc8536516f2d03b6
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2021-06-02 10:32:29 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Erik Salas on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
